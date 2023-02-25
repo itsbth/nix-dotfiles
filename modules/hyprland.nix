aa654fdfc4ca80f4e5b0ec9059df16183499f293
Bjørn Tore Håvie <itsbth@itsbth.com>
2023-02-26 00:13:03 +0100

    nixos: enable kitty

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -3,5 +3,6 @@
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
-   programs.hyprland.enable = true;
+  programs.hyprland.enable = true;
+  programs.kitty.enable = true;
 }
