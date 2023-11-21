7f8a2639bdb5a18f489ef3c4792481afe9b6832f
Bjørn Tore Håvie <itsbth@itsbth.com>
2023-11-21 12:19:04 +0100

    nix flake update(++)

--- hosts/Bjrns-MBP/configuration.nix
+++ hosts/Bjrns-MBP/configuration.nix
@@ -8,52 +8,60 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   # Put gui apps here to make them available in spotlight
-  environment.systemPackages =
-    [
-      pkgs.zstd
+  environment.systemPackages = [
+    pkgs.zstd
 
-      pkgs.neovim
-      # pkgs.kitty
-      pkgs.vscode
+    pkgs.neovim
+    # pkgs.kitty
+    pkgs.vscode
 
-      pkgs.nixpkgs-fmt
+    pkgs.nixpkgs-fmt
 
-      pkgs.jetbrains.goland
-      pkgs.jetbrains.webstorm
-      pkgs.jetbrains.clion
-      pkgs.jetbrains.pycharm-professional
-      pkgs.jetbrains.idea-ultimate
-      # not currently packaged
-      # pkgs.jetbrains.fleet
-      # pkgs.jetbrains.toolbox
+    pkgs.jetbrains.goland
+    (pkgs.jetbrains.webstorm.overrideAttrs {
+      version = "2023.2.5";
+      src = builtins.fetchurl {
+        url =
+          "https://download.jetbrains.com/webstorm/WebStorm-2023.2.5-aarch64.dmg";
+        sha256 =
+          "0skl7cqb3hgyz1sd7snsh7xcr54z530dqm7yyiisx1b6bs051p00";
+      };
+    })
+    pkgs.jetbrains.clion
+    pkgs.jetbrains.pycharm-professional
+    pkgs.jetbrains.idea-ultimate
+    # not currently packaged
+    # pkgs.jetbrains.fleet
+    # pkgs.jetbrains.toolbox
 
-      pkgs.discord
-      # pkgs.steam
+    pkgs.discord
+    # pkgs.steam
 
-      pkgs.iterm2
+    pkgs.iterm2
 
-      # ALL THE CONTAINERS (TODO: Trim this list)
-      # pkgs.podman
-      pkgs.qemu # needed for podman machine
-      pkgs.colima
-      pkgs.docker-client
-    ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "vscode"
-    "slack"
-
-    "goland"
-    "webstorm"
-    "clion"
-    "pycharm-professional"
-    "idea-ultimate"
-    # "fleet"
-    # "toolbox"
-
-    "discord"
-    "steam"
+    # ALL THE CONTAINERS (TODO: Trim this list)
+    # pkgs.podman
+    pkgs.qemu # needed for podman machine
+    pkgs.colima
+    pkgs.docker-client
   ];
+
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "vscode"
+      "slack"
+
+      "goland"
+      "webstorm"
+      "clion"
+      "pycharm-professional"
+      "idea-ultimate"
+      # "fleet"
+      # "toolbox"
+
+      "discord"
+      "steam"
+    ];
 
   fonts = {
     fontDir.enable = true;
