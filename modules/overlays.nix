6d39dd0b4683e3b06ec07ddfaf62f0e5ae48c407
Bjørn Tore Håvie <itsbth@itsbth.com>
2025-02-25 16:15:31 +0100

    fennel-ls is in nixpkgs, remove overlay

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -1,9 +1,5 @@
 {
   nixpkgs.overlays = [
-    (self: super: {
-      myougiden = self.callPackage ../packages/myougiden { };
-      fennel-ls = self.callPackage ../packages/fennel-ls { };
-      kitty = super.kitty.overrideAttrs (old: { doCheck = false; doInstallCheck = false; });
-    })
+    (self: super: { myougiden = self.callPackage ../packages/myougiden { }; })
   ];
 }
